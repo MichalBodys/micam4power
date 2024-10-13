dd1074f699574794583e98821a538e7c8af0ebad
MichalBodys <113517249+MichalBodys@users.noreply.github.com>
2024-10-13 09:56:42 +0200

    copyright back button

--- src/Sections/CopyrightCookies/CopyrightCookies.js
+++ src/Sections/CopyrightCookies/CopyrightCookies.js
@@ -1,34 +1,38 @@
 
 const CopyrightCookies = () => {
   return (
-    <div className="copyright-cookies h-lvh">
-      <h1>Copyright and Cookies</h1>
-      <p>
+    <div className="copyright-cookies h-lvh flex items-center flex-col mt-10 mx-5">
+      <h1 className=" text-lg">Copyright and Cookies</h1>
+      <p className="text-sm">
       Copyright Notice
         All content, including but not limited to text, images, graphics, logos, and code, available on this website is the property of [Your Company Name] and is protected by copyright laws. Unauthorized use, reproduction, or distribution of any materials on this website is prohibited unless expressly permitted by [Your Company Name] in writing.
 
         © [Year] [Your Company Name]. All rights reserved.
+        <br/>
 
         Cookies Policy
+        <br/>
         What Are Cookies?
+        <br/>
         Cookies are small text files stored on your device when you visit websites. They are used to improve your experience by remembering your preferences and providing personalized content.
-
+        <br/>
         Our Use of Cookies
         We only use essential cookies to ensure the website functions correctly. These cookies do not track or collect personal information.
-
+        <br/>
         Data Collection
         We collect personal data solely through forms where users voluntarily provide their email addresses for communication purposes. We do not use cookies for tracking or marketing purposes, nor do we collect any other personal data without your consent.
-
+        <br/>
         Changes to This Policy
         We reserve the right to modify or update this Cookies Policy at any time. Any changes will be posted on this page, and it is your responsibility to review this page periodically for updates. Continued use of the website after any changes constitutes your acceptance of the new policy.
-
+        <br/>
         Managing Cookies
         You can control the use of cookies through your browser settings. You may choose to block or delete cookies, but this may affect your experience on our website.
-
+        <br/>
         If you have any questions about this policy or how we handle your data, feel free to contact us at [Your Contact Email].
-
+        <br/>
         This additional clause about making changes allows you to modify the policy whenever necessary and notifies users to check back for updates. Let me know if you need further adjustments!
       </p>
+      <button  className="relative mt-10 text-sm py-2"><a href="/" className="py-3 px-10">Go back</a></button>
     </div>
   );
 };
